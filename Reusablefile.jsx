190a2cb41cff257c043bb103052c3072cc476b6d
Kumar Neupane <3278618@mbmc.edu.np>
2026-02-22 14:32:20 +0545

    change made

--- Reusablefile.jsx
+++ Reusablefile.jsx
@@ -32,8 +32,7 @@ const CreateProjectModal = ({ isOpen, onClose }) => {
             </div>
 
             {/* Description */}
-            
-<h1>hhdhd</h1>
+
             <div>
               <label className="block text-sm font-medium mb-1">
                 Description
